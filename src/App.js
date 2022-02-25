5d7b9633427c4b71c6552d9c77dca91c5e108e90
EC2 Default User <ec2-user@ip-172-31-11-217.ap-northeast-2.compute.internal>
2022-02-25 05:50:45 +0000

    Add new blog version

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App(props) {
     // eslint-disable-next-line
   }, [search]);
   
-  return (
+    return (
     <div className={classes.root}>
       <AppBar position="static" style={{ background: '#2E3B55' }}>
         <Toolbar>
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog
+            EKS DEMO Blog version 1
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
